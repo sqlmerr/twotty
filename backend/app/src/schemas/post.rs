4c7c45a22efb3dd98b2f10d89199c4fa5b65cf97
sqlmerr <sqlmerr@ya.ru>
2024-06-13 17:16:31 +0300

    😏 0.2.2 Column `created_at` for Post

--- backend/app/src/schemas/post.rs
+++ backend/app/src/schemas/post.rs
@@ -2,12 +2,14 @@ use crate::models::post::Post;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 use uuid::Uuid;
+use chrono::{NaiveDateTime};
 
 #[derive(Serialize, Deserialize, ToSchema, Debug, Clone)]
 pub struct PostSchema {
     pub id: Uuid,
     pub text: String,
     pub author_id: Uuid,
+    pub created_at: NaiveDateTime
 }
 
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
@@ -26,6 +28,7 @@ impl From<&Post> for PostSchema {
             id: value.clone().id,
             text: value.clone().text,
             author_id: value.author_id,
+            created_at: value.created_at,
         }
     }
 }
